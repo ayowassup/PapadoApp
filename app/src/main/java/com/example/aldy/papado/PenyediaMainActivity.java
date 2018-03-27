a9b71202c1d18c8e5a5b000e0ef9a8d0e16ad64c
aldyrizaldy9 <rizaldy.ilham@gmail.com>
2018-03-27 17:11:17 +0700

    asdf

--- app/src/main/java/com/example/aldy/papado/PenyediaMainActivity.java
+++ app/src/main/java/com/example/aldy/papado/PenyediaMainActivity.java
@@ -90,6 +90,10 @@ public class PenyediaMainActivity extends AppCompatActivity {
     //ini method yang melarang tombol kembali
     @Override
     public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
 //        super.onBackPressed();
     }
 }
