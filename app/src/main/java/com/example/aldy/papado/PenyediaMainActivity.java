12a990480ecc29f4656fa37796d603ab8a9b748e
aldyrizaldy9 <rizaldy.ilham@gmail.com>
2018-03-27 12:07:00 +0700

    asdf

--- app/src/main/java/com/example/aldy/papado/PenyediaMainActivity.java
+++ app/src/main/java/com/example/aldy/papado/PenyediaMainActivity.java
@@ -1,31 +1,62 @@
 package com.example.aldy.papado;
 
+import android.content.Intent;
 import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 
 public class PenyediaMainActivity extends AppCompatActivity {
+
     private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_penyedia_main);
-        mDrawerLayout = findViewById(R.id.drawer_layout);
 
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        mToolbar = findViewById(R.id.penyedia_nav_action);
+        setSupportActionBar(mToolbar);
+
+        mDrawerLayout = findViewById(R.id.penyedia_drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView navigationView = findViewById(R.id.penyedia_nav_view);
         navigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(MenuItem menuItem) {
-                        // set item as selected to persist highlight
-                        menuItem.setChecked(true);
+//                         set item as selected to persist highlight
+//                        menuItem.setChecked(true);
+
+                        switch (menuItem.getItemId()) {
+                            case R.id.penyedia_nav_halamansaya:
+                                Intent halamansaya = new Intent(PenyediaMainActivity.this, HalamanSayaActivity.class);
+                                startActivity(halamansaya);
+                                break;
+                            case R.id.penyedia_nav_pemesanan:
+                                Intent pemesanan = new Intent(PenyediaMainActivity.this, PemesananActivity.class);
+                                startActivity(pemesanan);
+                                break;
+                            case R.id.penyedia_nav_pengaturan:
+                                Intent pengaturan = new Intent(PenyediaMainActivity.this, PengaturanActivity.class);
+                                startActivity(pengaturan);
+                                break;
+                            case R.id.penyedia_nav_keluar:
+                                //kode kalau dia tekan logout
+                                break;
+                        }
+
                         // close drawer when item is tapped
                         mDrawerLayout.closeDrawers();
 
@@ -36,45 +67,19 @@ public class PenyediaMainActivity extends AppCompatActivity {
                     }
                 });
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        ActionBar actionbar = getSupportActionBar();
-        actionbar.setDisplayHomeAsUpEnabled(true);
-        actionbar.setHomeAsUpIndicator(R.drawable.logo);
-
-        mDrawerLayout.addDrawerListener(
-                new DrawerLayout.DrawerListener() {
-                    @Override
-                    public void onDrawerSlide(View drawerView, float slideOffset) {
-                        // Respond when the drawer's position changes
-                    }
-
-                    @Override
-                    public void onDrawerOpened(View drawerView) {
-                        // Respond when the drawer is opened
-                    }
-
-                    @Override
-                    public void onDrawerClosed(View drawerView) {
-                        // Respond when the drawer is closed
-                    }
-
-                    @Override
-                    public void onDrawerStateChanged(int newState) {
-                        // Respond when the drawer motion state changes
-                    }
-                }
-        );
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                mDrawerLayout.openDrawer(GravityCompat.START);
-                return true;
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
+    //ini method yang melarang tombol kembali
+    @Override
+    public void onBackPressed() {
+//        super.onBackPressed();
+    }
 }
